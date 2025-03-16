359f53cb9214d053a406f32a017bcde669d833a5
DaoNguyen <daonguyen2941@gmail.com>
2025-03-16 11:52:08 +0700

    update managerClientService

--- src/redis/services/managerClient.service.ts
+++ src/redis/services/managerClient.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { RedisCacheService } from 'src/redis/services/redisCache.service';
 import { IExtendUserInSocket, IUserInSocket } from 'src/common/Interface';
-
+import { enumUserStatus } from 'src/modules/chat/dto/chat.dto';
 @Injectable()
 export class ManagerClientSocketService {
   private readonly SESSION_PREFIX = 'ws_session:'; // Tiền tố cho session WebSocket
 
   constructor(private readonly cacheService: RedisCacheService) {}
+
+  async UserStatus(userId: string): Promise<enumUserStatus> {
+    const session = await this.cacheService.getCache<IUserInSocket>(`${this.SESSION_PREFIX}${userId}`);
+    console.log(session);
+    
+    return session ? enumUserStatus.online : enumUserStatus.offline // Nếu có session => online, nếu không => offline
+  }
 
   async addClientSocket(userId: string, value: IUserInSocket): Promise<void> {
     await this.cacheService.setCache(`${this.SESSION_PREFIX}${userId}`, value); // Hết hạn sau 1 giờ
