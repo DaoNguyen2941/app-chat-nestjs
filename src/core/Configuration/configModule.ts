8be972559299dbdfa3dde57f0394becaf6767ec6
DaoNguyen <daonguyen2941@gmail.com>
2025-12-02 10:28:18 +0700

    update connect

--- src/core/Configuration/configModule.ts
+++ src/core/Configuration/configModule.ts
@@ -39,6 +39,8 @@ import { Module } from '@nestjs/common';
         REDIS_PORT: Joi.number().required().default(6379),
         REDIS_DB: Joi.string(),
         REDIS_PASSWORD: Joi.string().optional(),
+        REDIS_USER: Joi.string().optional(),
+        REDIS_URL: Joi.string().optional(),
         //aws-s3
         CLOUDFLARE_ACCESS_KEY_ID: Joi.string().required(),
         CLOUDFLARE_SECRET_ACCESS_KEY: Joi.string().required(),
